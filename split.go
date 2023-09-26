f6751f6e0e142ca284b8b032f22abedb031f4e3b
Munkyu Im <johnny@daangn.com>
2023-09-26 12:43:30 +0900

    add annotation to explain the mapping of special char and decimal

--- split.go
+++ split.go
@@ -35,7 +35,42 @@ func SplitToString(str string, opt SplitOpt, ignoreSeparators, ignoreCases, igno
 			}
 			continue
 		}
-
+		/*
+				ASCII
+			Character 	Decimal
+				!		33
+				"		34
+				#		35
+				$		36
+				%		37
+				&		38
+				'		39
+				(		40
+				)		41
+				*		42
+				+		43
+				,		44
+				-		45
+				.		46
+				/		47
+				:		58
+				;		59
+				<		60
+				=		61
+				>		62
+				?		63
+				@		64
+				[		91
+				\		92
+				]		93
+				^		94
+				_		95
+				`		96
+				{		123
+				|		124
+				}		125
+				~		126
+		*/
 		if ignoreSpecialChars == true {
 			if hex >= 33 && hex <= 47 {
 				continue
